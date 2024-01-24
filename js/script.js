b022af10b2ed08ecc35f6fd43455635fd5fd2828
ani-Avoir <roy.aniruddha@gmail.com>
2024-01-25 01:25:18 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,3 +22,30 @@ AOS.init({
            
          });
 
+$('input.value:first-child').val("$" + 0 + "-");
+         $('input.value:nth-child(2)').val("$" + 500 );
+
+         $('#slider-range').slider({
+              min: 0,
+              max: 500,
+              step: 1,
+              values: [ 0  , 500],
+              range: true,
+              slide: function(event, ui) {
+                $.each(ui.values, function(i, v) {
+                  if( i == 0 )  {
+                  $('input.value').eq(i).val("$" + v + "-");
+                } else {
+                  $('input.value').eq(i).val("$" + v);
+                }
+                });
+    }
+  });
+
+         $('input.value').change(function() {
+    var $this = $(this);
+    $('#slider-range').slider('values', $this.attr('data-index'), $this.val());
+  });
+
+
+
